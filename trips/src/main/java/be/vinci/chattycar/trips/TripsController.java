3c1d742178b2766c8bb9bf429dc592e6f4963d77
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-11-21 15:50:43 +0100

    Add: getAll

--- trips/src/main/java/be/vinci/chattycar/trips/TripsController.java
+++ trips/src/main/java/be/vinci/chattycar/trips/TripsController.java
@@ -2,10 +2,16 @@ package be.vinci.chattycar.trips;
 
 import be.vinci.chattycar.trips.models.NewTrip;
 import be.vinci.chattycar.trips.models.Trip;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,6 +22,17 @@ public class TripsController {
 
   public TripsController(TripsService service) {
     this.service = service;
+  }
+
+  @GetMapping("/trips")
+  public ResponseEntity<List<Trip>> getAll(
+      @RequestParam(required = false) LocalDate departureDate,
+      @RequestParam(required = false) double originLat,
+      @RequestParam(required = false) double originLon,
+      @RequestParam(required = false) double destinationLat,
+      @RequestParam(required = false) double destinationLon
+  ) {
+    return new ResponseEntity<>(new ArrayList<Trip>(), HttpStatus.ACCEPTED);
   }
 
   @PostMapping("/trips")
