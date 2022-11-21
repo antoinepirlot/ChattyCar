17f3263e61295e532bca2a03c3d8d4d5b0215e45
Stefan <stefan.mircovici@student.vinci.be>
2022-11-21 14:24:54 +0100

    init notifications service

---
+++ notifications/src/test/java/be/vinci/chattycar/notifications/NotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.chattycar.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
