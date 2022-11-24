3b701706775461eafdafa2f9c63eadb960f95286
Stefan <stefan.mircovici@student.vinci.be>
2022-11-24 22:35:38 +0100

    [ADD] notifications repository

---
+++ notifications/src/main/java/be/vinci/chattycar/notifications/data/NotificationsRepository.java
@@ -0,0 +1,8 @@
+package be.vinci.chattycar.notifications.data;
+
+import be.vinci.chattycar.notifications.models.Notification;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NotificationsRepository extends CrudRepository<Notification, Integer> {
+
+}
