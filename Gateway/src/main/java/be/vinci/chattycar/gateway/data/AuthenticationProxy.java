6161cfb2c3f49550842a28c056ac458594932502
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-26 12:49:42 +0100

    [ADD] complete the authproxy

--- Gateway/src/main/java/be/vinci/chattycar/gateway/data/AuthenticationProxy.java
+++ Gateway/src/main/java/be/vinci/chattycar/gateway/data/AuthenticationProxy.java
@@ -1,14 +1,28 @@
 package be.vinci.chattycar.gateway.data;
 
 import be.vinci.chattycar.gateway.models.Credentials;
+import be.vinci.chattycar.gateway.models.InsecureCredentials;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @Repository
 @FeignClient(name = "authentication")
 public interface AuthenticationProxy {
   @PostMapping("/authentication/connect")
   String connect(@RequestBody Credentials credentials);
+
+  @PostMapping("/authentication/verify")
+  String verify(@RequestBody String token);
+
+  @PostMapping({"/authentication/{email}"})
+  ResponseEntity<Void> createOne(@PathVariable String email, @RequestBody InsecureCredentials credentials);
+
+  @PutMapping({"/authentication/{email}"})
+  void updateOne(@PathVariable String email, @RequestBody InsecureCredentials credentials);
+
+  @DeleteMapping({"/authentication/{email}"})
+  void deleteCredentials(@PathVariable String email);
+
 }
