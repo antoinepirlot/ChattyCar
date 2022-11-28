b538cef0753524f38edee7799ca6421fd68e50e0
Nicolas Poppe <nicolas.poppe@student.vinci.be>
2022-11-28 17:56:08 +0100

    [REFACTOR] change id to tripId

--- Gateway/src/main/java/be/vinci/chattycar/gateway/data/PassengersProxy.java
+++ Gateway/src/main/java/be/vinci/chattycar/gateway/data/PassengersProxy.java
@@ -2,17 +2,21 @@ package be.vinci.chattycar.gateway.data;
 
 import be.vinci.chattycar.gateway.models.Passengers;
 import be.vinci.chattycar.gateway.models.Trip;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Repository
+@FeignClient(name = "passengers")
 public interface PassengersProxy {
 
     @GetMapping("/passengers/{tripId}")
-    List<Passengers> getAllPassengersFromATrip(@PathVariable int id);
+    List<Passengers> getAllPassengersFromATrip(@PathVariable int tripId);
 
     @PostMapping("/passengers/{tripId}")
-    void addPassengerToATrip(@PathVariable int id, @RequestBody Passengers passenger);
+    void addPassengerToATrip(@PathVariable int tripId, @RequestBody Passengers passenger);
 
     @PutMapping("/passengers/{id}")
     void updatePassenger(@PathVariable int id, @RequestBody Passengers passenger);
