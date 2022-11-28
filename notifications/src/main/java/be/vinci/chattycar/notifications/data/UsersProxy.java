abc5db06d95ef3b48180316464bdea9f6c1a057c
Stefan <stefan.mircovici@student.vinci.be>
2022-11-28 14:30:08 +0100

    [ADD] users proxy and user model into notifications

--- notifications/src/main/java/be/vinci/chattycar/notifications/data/UsersProxy.java
+++ notifications/src/main/java/be/vinci/chattycar/notifications/data/UsersProxy.java
@@ -1,5 +1,6 @@
 package be.vinci.chattycar.notifications.data;
 
+import be.vinci.chattycar.notifications.models.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Repository
 @FeignClient(name = "users")
-public class UsersProxy {
+public interface UsersProxy {
 
-
+  @GetMapping("/users/{id}")
+  User readOneById(@PathVariable int id);
 }
