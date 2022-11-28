30592e2c9d463cb1bfaa381d30ba03b5f0f9e0bd
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-11-28 18:49:22 +0100

    Init: position service

---
+++ Positions/src/test/java/be/vinci/chattycar/positions/PositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vinci.chattycar.positions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PositionsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
