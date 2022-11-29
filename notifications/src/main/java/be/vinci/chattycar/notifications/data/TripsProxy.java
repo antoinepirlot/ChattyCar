aee7108a19f61db1fc02f464df85505caa626fa5
Stefan <stefan.mircovici@student.vinci.be>
2022-11-29 12:19:45 +0100

    [ADD] trips proxy

---
+++ notifications/src/main/java/be/vinci/chattycar/notifications/data/TripsProxy.java
@@ -0,0 +1,15 @@
+package be.vinci.chattycar.notifications.data;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "trips")
+public interface TripsProxy {
+
+  @GetMapping("/trips/{id}")
+  Trip getOneById(@PathVariable int id);
+
+}
