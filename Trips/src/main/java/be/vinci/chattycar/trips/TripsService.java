fe57c906249d4c62cb818d039b7e76e0cdb3ffb4
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-11-29 17:13:14 +0100

    Update: getAll

--- Trips/src/main/java/be/vinci/chattycar/trips/TripsService.java
+++ Trips/src/main/java/be/vinci/chattycar/trips/TripsService.java
@@ -5,6 +5,7 @@ import be.vinci.chattycar.trips.data.PositionsProxy;
 import be.vinci.chattycar.trips.data.TripsRepository;
 import be.vinci.chattycar.trips.models.NewTrip;
 import be.vinci.chattycar.trips.models.PassengerTrips;
+import be.vinci.chattycar.trips.models.Position;
 import be.vinci.chattycar.trips.models.Trip;
 import java.time.LocalDate;
 import java.util.List;
@@ -38,12 +39,59 @@ public class TripsService {
     return res;
   }
 
-  public List<Trip> getAll(LocalDate departureDate, double originLat, double originLon,
-      double destinationLat, double destinationLon) {
-    List<Trip> trips = this.repository.getTripsByOrderByIdDesc();
-    List<Trip> trips1 = trips.stream().filter(t -> t.getAvailableSeating() > 0).toList();
+  public List<Trip> getAll(LocalDate departureDate, Double originLat, Double originLon,
+      Double destinationLat, Double destinationLon) {
+    List<Trip> trips = null;
+    Position origin = new Position();
+    Position destination = new Position();
+    origin.setLongitude(originLon);
+    origin.setLatitude(originLat);
+    destination.setLongitude(destinationLon);
+    destination.setLatitude(destinationLat);
+    if (departureDate == null) {
+      if (originLat == null && originLon == null) { // No departure date & No origin
+        if (destinationLat == null && destinationLon == null) { // No departure date & No origin & No destination
+          trips = this.repository.getTripsByAvailableSeatingGreaterThanOrderByIdDesc(0);
+          return trips.stream().filter(t -> t.getDeparture().isAfter(LocalDate.now())).toList();
+        }
+        if(destinationLat != null && destinationLon != null) { // No departure date & No oigin & Destination
+          trips = this.repository.getTripsByAvailableSeatingGreaterThanAndDestinationEqualsOrderByIdDesc(0, destination);
+        }
+      }
+      if (originLon != null && originLat != null) { // No departure date & Origin
+        if (destinationLat == null && destinationLon == null) { // No departure date & Origin & No destination
+          trips = this.repository.getTripsByAvailableSeatingGreaterThanAndOriginEqualsOrderByIdDesc(0, origin);
+          return trips.stream().filter(t -> t.getDeparture().isAfter(LocalDate.now())).toList();
+        }
+        if(destinationLat != null && destinationLon != null) { // No departure date & Origin & Destination
+          trips = this.repository.getTripsByAvailableSeatingGreaterThanAndOriginEqualsAndDestinationEqualsOrderByIdDesc(0, origin, destination);
+        }
+      }
+    }
     //TODO calcul distance avec positions qui calcule la distance
     return trips;
+  }
+
+  private List<Trip> filterTrips(List<Trip> trips, Position position) {
+    return trips.stream()
+        .filter(t -> t.getOrigin().equals(position))
+        .sorted((t1, t2) -> {
+          double t1StartLon = t1.getOrigin().getLongitude();
+          double t1StartLat = t1.getOrigin().getLatitude();
+          double t1EndLon = t1.getOrigin().getLongitude();
+          double t1EndLat = t1.getOrigin().getLatitude();
+
+          double t2StartLon = t2.getOrigin().getLongitude();
+          double t2StartLat = t2.getOrigin().getLatitude();
+          double t2EndLon = t2.getOrigin().getLongitude();
+          double t2EndLat = t2.getOrigin().getLatitude();
+
+          int dist1 = this.positionsProxy.getDistance(t1StartLon, t1EndLon, t1StartLat, t1EndLat);
+          int dist2 = this.positionsProxy.getDistance(t2StartLon, t2EndLon, t2StartLat, t2EndLat);
+
+          return dist1 - dist2;
+        })
+        .toList();
   }
 
   /**
