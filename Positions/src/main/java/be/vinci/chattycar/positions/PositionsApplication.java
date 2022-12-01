f8b5b08b764fe042c0c307545ca7d1854789337c
Antoine Pirlot <pirlotantoine2@gmail.com>
2022-12-01 11:07:45 +0100

    Fix: positions connection issues

--- Positions/src/main/java/be/vinci/chattycar/positions/PositionsApplication.java
+++ Positions/src/main/java/be/vinci/chattycar/positions/PositionsApplication.java
@@ -2,12 +2,10 @@ package be.vinci.chattycar.positions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
+@EnableDiscoveryClient
 public class PositionsApplication {
 
   public static void main(String[] args) {
